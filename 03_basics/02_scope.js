456f31c24b0192230f9f38b2d492157d696710e6
Pratik168960 <pratiksingh111252@gmail.com>
2026-05-31 16:17:49 +0530

    scope in js

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,18 @@
+// let a = 10 
+// const b = 20 
+// var c = 30 
+
+if (true) {
+    let a = 10 
+    const b = 20 
+    console.log("INNER: ", a)
+}
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+// the global scope in a browser window and 
+// a Node.js code environment are different
+
